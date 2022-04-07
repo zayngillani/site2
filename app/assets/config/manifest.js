71bae531ee6158e2e18f4dacbde2d32ee3668cb7
zayn <zayngillani017@gmail.com>
2022-04-07 17:27:25 +0500

    remove link

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,3 @@
 //= link_tree ../builds
 //= link application.css
 //= link application.js
-
-//= link_tree ../javascript .js
-//= link_tree ../../../vendor/javascript .js
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,5 @@
 //= require jquery
 //= require bootstrap.min
 //= require jquery_ujs
-//= require jquery_ui
-//= require turbolinks
 //= require popper
 //= require selectize
-
-
-debugger
